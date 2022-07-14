7a09e47d00fb095ebe2b233e856cff3ebbce0d2c
saiprakashpatnala <saiprakashpatnala@gmail.com>
2022-07-14 05:26:11 +0000

    scoreboard

---
+++ src/components/Scoreboard/index.js
@@ -0,0 +1,38 @@
+import {useState, useEffect} from 'react'
+
+import {
+  MainContainer,
+  ScoreText,
+  Button,
+  ButtonsContainer,
+} from './styledComponents'
+
+const Scoreboard = () => {
+  const [score, setScore] = useState(0)
+
+  const effect = () => {
+    document.title = `Score: ${score}`
+  }
+
+  useEffect(effect)
+
+  const onIncrement = () => {
+    setScore(prevScore => prevScore + 1)
+  }
+
+  const onDecrement = () => {
+    setScore(prevScore => prevScore - 1)
+  }
+
+  return (
+    <MainContainer>
+      <ScoreText>{score}</ScoreText>
+      <ButtonsContainer>
+        <Button onClick={onIncrement}>Increase</Button>
+        <Button onClick={onDecrement}>Decrease</Button>
+      </ButtonsContainer>
+    </MainContainer>
+  )
+}
+
+export default Scoreboard
